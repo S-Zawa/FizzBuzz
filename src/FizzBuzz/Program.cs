cdf724157bd46bd0b159869eababfba139fa37c5
S.Aizawa <showta4696@gmail.com>
2023-01-08 22:50:54 +0900

    ハードコーディングでFizzBuzz実装

--- src/FizzBuzz/Program.cs
+++ src/FizzBuzz/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace FizzBuzz // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var value = Console.ReadLine();
+            Console.WriteLine(FizzBuzz(value));
+        }
+
+        public static string FizzBuzz(string value)
+        {
+            var number = 0;
+
+            try
+            {
+                number = int.Parse(value);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine($"Unable to parse '{value}'");
+                return "";
+            }
+
+            if (number % 15 == 0)
+            {
+                return "FizzBuzz";
+            }
+
+            if (number % 3 == 0)
+            {
+                return "Fizz";
+            }
+
+            if (number % 5 == 0)
+            {
+                return "Buzz";
+            }
+
+            return number.ToString();
+        }
+    }
+}
